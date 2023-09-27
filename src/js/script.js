cba378731ea4de4fe92daa7ecb66b323e1963b1b
Anderson Freitas <freitas.dev@proton.me>
2023-09-26 23:52:00 -0300

    :rocket: First Commit
    First Commit

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+$('#option-gojo').on('click',function(event){
+    removeAll()
+    addClasses('gojo')
+});
+
+$('#option-kento').on('click',function(event){
+    removeAll()
+    addClasses('kento')
+});
+
+$('#option-megumi').on('click',function(event){
+    removeAll()
+    addClasses('megumi')
+});
+
+$('#option-nobara').on('click',function(event){
+    removeAll()
+    addClasses('nobara')
+});
+
+$('#option-sukuna').on('click',function(event){
+    removeAll()
+    addClasses('sukuna')
+});
+
+function removeAll(){
+    // console.log('removendo')
+    $('#gojo').addClass('display-none')
+    $('#kento').addClass('display-none')
+    $('#megumi').addClass('display-none')
+    $('#nobara').addClass('display-none')
+    $('#sukuna').addClass('display-none')
+
+    $('#option-menu-gojo').removeClass('option-selected')
+    $('#option-menu-kento').removeClass('option-selected')
+    $('#option-menu-megumi').removeClass('option-selected')
+    $('#option-menu-nobara').removeClass('option-selected')
+    $('#option-menu-sukuna').removeClass('option-selected')
+}
+
+function addClasses(nome){
+    // console.log('adicionando')
+    $(`#${nome}`).removeClass('display-none')
+    $(`#option-menu-${nome}`).addClass('option-selected')
+}
